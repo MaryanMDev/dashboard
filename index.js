d3b0229896192b43a099bac30e46670b16acefe5
Maryan <maryanmodev@gmail.com>
2022-01-17 06:31:10 +0000

    fixing links

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ dotenv.config({path: "./config/config.env"});
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+});
 
 app.listen(PORT, (req , res) => {
     console.log(`server running on port ${PORT}`);
